3b0621e96521e7e6c11b5f37a3b67098fe3aff17
Daouda <daouda.sene@aims-senegal.org>
2026-09-16 09:44:12 +0000

    Update llm_engine.py

--- src/llm_engine.py
+++ src/llm_engine.py
@@ -31,7 +31,7 @@ ANSWER:
 """
 
     response = client.chat.completions.create(
-        model="llama-3.1-8b-instant",
+        model="llama-3.3-70b-versatile",
         messages=[
             {
                 "role": "user",
